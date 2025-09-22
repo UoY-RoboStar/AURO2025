558ce65301d0323d8b904930cf38c52acb99f5e2
Pedro Ribeiro <pedro.ribeiro@york.ac.uk>
2025-09-22 15:38:11 +0100

    Added new content and updated gitignore.

---
+++ week_1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'week_1'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ubuntu',
+    maintainer_email='pedro.ribeiro@york.ac.uk',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'turtlebot3_drive_python = week_1.turtlebot3_drive_python:main'
+        ],
+    },
+)
